23511bf3a53cf85c00a9fc13ea61252aa43237df
ferre-vaes <devferre@gmail.com>
2020-10-17 16:32:39 +0200

    Started on the checkout process

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,6 +1,7 @@
 import { 
   CART_ADD_ITEM,
-  CART_REMOVE_ITEM
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS
 } from '../constants/cartConstants'
 import axios from 'axios'
 
@@ -29,4 +30,13 @@ export const removeFromCart = (id) => async (dispatch, getState) => {
   })
 
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
+
+export const saveShippingAddress = (data) => async (dispatch) => {
+  dispatch({
+    type: CART_SAVE_SHIPPING_ADDRESS,
+    payload: data
+  })
+
+  localStorage.setItem('shippingAddress', JSON.stringify(data))
 }
